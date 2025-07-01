e3d76c2e2b521f9af3dbc002ecae862c424d511c
Ralph Martinez <rafael.adan.martinez@gmail.com>
2025-07-01 10:31:43 -0600

    Add ProtonVPN

--- configuration.nix
+++ configuration.nix
@@ -112,6 +112,7 @@
     zsh
     google-chrome
     caffeine-ng
+    protonvpn-gui
   ];
 
   #programs.zsh.enable = true;
@@ -136,6 +137,7 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+  networking.firewall.checkReversePath = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
